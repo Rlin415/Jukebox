19ef14891006ee1dc3ede966e6ae4f4a184a056e
Robert Lin <Rlin415@gmail.com>
2015-12-23 22:08:20 -0800

    designed basic UI and set up sekelton of the view using react

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,45 @@
-// Your ReactJS code here
+var TrackContainer = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      <div className='trackContainer'>
+        <h2> List of songs matching your request </h2>
+        <TrackList data={this.state.data} />
+      </div>
+    );
+  }
+});
 
+var TrackList = React.createClass({
+  render: function() {
+    var trackNodes = this.props.data.map(function(track) {
+      return (
+        <Track title={track.title}></Track>
+      );
+    });
+    return (
+      <div className='trackList'>
+        {trackNodes}
+      </div>
+    );
+  }
+});
+
+var Track = React.createClass({
+  render: function() {
+    return (
+      <div className='track'>
+        <h4 className='trackArtist'>
+          {this.props.title}
+        </h4>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <TrackContainer />,
+  document.getElementById('content')
+);
